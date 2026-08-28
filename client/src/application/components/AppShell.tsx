f851b019d4f37b3e27f8f992fdb1fe668fa8c5db
Tarte12 <101166683+Tarte12@users.noreply.github.com>
2026-08-28 21:48:55 +0900

    refactor(client): connect notes through app shell

--- client/src/application/components/AppShell.tsx
+++ client/src/application/components/AppShell.tsx
@@ -10,7 +10,8 @@ import {
   useWindowDimensions,
   View,
 } from "react-native";
-import { MemoScreen, TimeScreen } from "../../Screens";
+import { TimeScreen } from "../../Screens";
+import { MemoScreen } from "../../notes/components/MemoScreen";
 import { TodoScreen } from "../../todo/components/TodoScreen";
 import type { useAppData } from "../../useAppData";
 import { styles } from "./AppShell.styles";
